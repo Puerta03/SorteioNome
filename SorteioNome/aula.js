86e110cbd8f6ef8740cc662c725de52f1b959fde
Puerta03 <133425034+Puerta03@users.noreply.github.com>
2023-09-21 15:12:17 -0300

    Add files via upload

---
+++ SorteioNome/aula.js
@@ -0,0 +1,15 @@
+console.log(Math.random());
+console.log(Math.random() * 20);
+console.log(Math.random(7 - 3 + 1)-3);
+
+//Arredonda para o valor inteiro mais próximo
+console.log(Math.random(3.1));
+console.log(Math.random(3.9));
+
+//Arredonda para baixo
+console.log(Math.random(3.1));
+console.log(Math.random(3.9));
+
+//Arredonda para cima
+console.log(Math.ceil(3.1))
+console.log(Math.ceil(3.9))
---
+++ SorteioNome/main.js
@@ -0,0 +1,25 @@
+const btnSortear = document.querySelector(".sortear");
+const textarea = document.querySelector("textarea");
+const tagResultado = document.querySelector(".resultado p");
+const popUpResultado = document.querySelector(".resultado");
+
+function pegarDadosFormulario() {
+    const valorCamp = textarea.value;
+    const listaNomes = valorCamp.split(",");
+    const tamanhoArray = listaNomes.length;
+    const posicaoNome = gerarNomeAleatorio(tamanhoArray);
+    const nome = listaNomes[posicaoNome];
+    mostrarResultado(nome);
+
+    textarea.value = ""
+}
+btnSortear.addEventListener("click", pegarDadosFormulario)
+
+function gerarNomeAleatorio(tamanhoArray) {
+return Math.floor(Math.random()*(tamanhoArray - 0) + 0)
+}
+
+function mostrarResultado(nome) {
+    tagResultado.innerHTML = `Resultado: ${nome}`
+    popUpResultado.classList.add("mostrarResultado")
+}
